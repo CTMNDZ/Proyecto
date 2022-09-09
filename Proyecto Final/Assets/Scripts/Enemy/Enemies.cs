a9b7e3702117e109b3b0cbdea961a7e2d3336a62
CTMNDZ <73859018+CTMNDZ@users.noreply.github.com>
2022-09-08 22:23:35 -0300

    Herencia y scriptable objects

---
+++ Proyecto Final/Assets/Scripts/Enemy/Enemies.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    [SerializeField]
+    protected EnemyData enemyData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Attack();
+    }
+
+    public void Attack()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up, transform.TransformDirection(Vector3.forward), out hit,enemyData.RangeAttack))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.Log("ATACAR AL JUGADOR");
+            }
+        }
+    }
+    protected virtual void Move()
+    {
+        transform.Translate(Vector3.forward * enemyData.speed * Time.deltaTime);
+    }
+
+    public void DrawRaycast()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 directionRay = transform.TransformDirection(Vector3.forward) * enemyData.RangeAttack;
+        Gizmos.DrawRay(transform.position + Vector3.up, directionRay);
+    }
+
+    private void OnDrawGizmos() {
+        DrawRaycast();
+    }
+}
